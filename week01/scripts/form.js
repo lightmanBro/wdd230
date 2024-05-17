c1c1001e387d777135a900466ba4461a32a31f8a
LightmanBro <omotosodavidromeo@gmail.com>
2024-05-17 08:06:42 +0100

    form assignment with submitted page fixed

--- week01/scripts/form.js
+++ week01/scripts/form.js
@@ -8,3 +8,25 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+
+
+const kp1 = document.querySelector("#password");
+const kp2 = document.querySelector("#password_confirm");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗ Passwords DO NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
