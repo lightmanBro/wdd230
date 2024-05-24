a1b02294f82aaceac774bce50760ac928c6c773d
LightmanBro <omotosodavidromeo@gmail.com>
2024-05-24 15:09:56 +0100

    ....

--- scripts/links.js
+++ scripts/links.js
@@ -42,3 +42,33 @@ function displayLinks(weeks) {
 }
 
 getLinks();
+
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const apiKey = '38468ba944f232ca60c55c5b55943ed4';
+const city = 'Lagos'; // Specify the city
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch(){
+    try {
+        const response = await fetch(url);
+        if(response.ok){
+            const data = await response.json();
+            displayResults(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    let desc = data.weather[0].description;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F - ${ desc.includes("sunshine") ? "🌞" : "☔"} ${desc}`;
+}
